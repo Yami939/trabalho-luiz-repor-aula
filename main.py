6643ca652969321878bd9118599a62c1bc4d0ba2
Yami939 <150692981+Yami939@users.noreply.github.com>
2024-10-04 12:19:28 -0300

    trabalho pronto

---
+++ main.py
@@ -0,0 +1,13 @@
+print ('INFORMAÇÕES USUARIO')
+
+print ('Olá Usuario, poderia informe seu nome:')
+
+nome_do_usuario = input('Digite o seu nome aqui: ')  
+
+print ('Obrigado,', nome_do_usuario ,',agora informe sua idade ')
+
+idade_do_usuario = (input('Digite a sua idade'))
+
+print (idade_do_usuario)
+
+print ('Olá ', nome_do_usuario, 'vejo que voce tem', idade_do_usuario ,'anos, bem vindo')
